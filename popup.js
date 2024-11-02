8806ef08708991a510a433f959ecc71026236818
jbc-sujon <jbc.sujon@gmail.com>
2024-11-02 21:26:36 +0600

    All files are added

---
+++ popup.js
@@ -0,0 +1,13 @@
+document.getElementById("changeColor").addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: changeBackgroundColor
+      });
+    });
+  });
+  
+  function changeBackgroundColor() {
+    document.body.style.backgroundColor = "lightblue";
+  }
+  
